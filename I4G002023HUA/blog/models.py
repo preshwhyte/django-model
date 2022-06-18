5ac265337c410928b81fa87425ca1c33ae8debc6
unknown <preshwhyte1@gmail.com>
2022-06-18 12:08:26 +0100

    creates model

---
+++ I4G002023HUA/blog/models.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Post(models.Model):
+    Title=models.CharField(max_length=200)
+    Text=models.TextField(max_length=500)
+    Author=models.ForeignKey(User, related_name='author', on_delete=models.CASCADE)
+    Created_date=models.DateTimeField(auto_now_add=True)
+    Published_date=models.DateTimeField(default=datetime.today)
+
+    def __str__(self):
+        return self.Title
