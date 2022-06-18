7f44e9705a2c01cfd763cd58675bc55956309b5d
Precious Nwogu <62227120+preshwhyte@users.noreply.github.com>
2022-06-18 23:35:41 +0100

    Update models.py

--- I4G002023HUA/blog/models.py
+++ I4G002023HUA/blog/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     Title=models.CharField(max_length=200)
-    Text=models.TextField(max_length=500)
+    Text=models.TextField()
     Author=models.ForeignKey(User, related_name='author', on_delete=models.CASCADE)
     Created_date=models.DateTimeField(auto_now_add=True)
     Published_date=models.DateTimeField(default=datetime.today)
